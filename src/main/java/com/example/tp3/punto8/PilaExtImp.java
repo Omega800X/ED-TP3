a39b9913b6196215546b392e64c61a4e245f9b8c
fernero4 <hortafer02@gmail.com>
2021-10-03 19:48:41 -0300

    arrreglando el 8

--- src/main/java/com/example/tp3/punto8/PilaExtImp.java
+++ src/main/java/com/example/tp3/punto8/PilaExtImp.java
@@ -1,9 +1,6 @@
 
 package punto8;
-import ar.edu.uner.fcad.ed.edlineales.NodoLista;
-import ar.edu.uner.fcad.ed.edlineales.colas.Cola;
 import ar.edu.uner.fcad.ed.edlineales.colas.ColaPorEnlaces;
-import ar.edu.uner.fcad.ed.edlineales.pilas.Pila;
 import ar.edu.uner.fcad.ed.edlineales.pilas.PilaPorEnlaces;
 
 
@@ -22,31 +19,24 @@ public class PilaExtImp <T> extends PilaPorEnlaces implements PilaExtInterfaz<T>
      * @param pilaParam */
     @Override
     public void agregarTodos(PilaPorEnlaces <T> pilaParam){
-        
-        while (!pilaParam.isEmpty()){
+      while (!pilaParam.isEmpty()){
             this.push(pilaParam.top());
-        }      
+            pilaParam.pop();
+        }        
     
     }
     
-    /** Invierte el orden de los elementos de la estructura. 
-     ---------------------------revisar-----------------------------------------------------tope.getElemento()
-     */
+    /** Invierte el orden de los elementos de la estructura. */
     @Override
     public void invertirOrden(){
-        ColaPorEnlaces<T> aux1= new ColaPorEnlaces();
-//        NodoLista tope1= this.tope;
-//        PilaPorEnlaces<T> aux = new PilaPorEnlaces();
+        ColaPorEnlaces<T> aux= new ColaPorEnlaces();
         while(!this.isEmpty()){
-            aux1.enqueue((T) this.top());
+            aux.enqueue((T) this.top());
             this.pop();
-//            aux.push((T) this.top());
-//            this.tope=this.tope.getSiguiente();
         }
-//        this.tope=tope1;
-        while(!aux1.isEmpty()){
-            this.push(aux1.getFront());
-            aux1.dequeue();
+        while(!aux.isEmpty()){
+            this.push(aux.getFront());
+            aux.dequeue();
         }
 
         }
